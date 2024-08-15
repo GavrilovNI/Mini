bbf36daf97169fad041cb3ca7e34ab2726cf1784
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-08-16 00:19:37 +0200

    remove ProxyDisabled component, disable components by ProxyDisabler instead of whole gameobject

---
+++ code/Networking/ProxyDisabler.cs
@@ -0,0 +1,31 @@
+﻿using Sandbox;
+using System.Collections.Generic;
+
+namespace Mini.Networking;
+
+public sealed class ProxyDisabler : Component, Component.INetworkSpawn
+{
+    [Property]
+    public List<Component> ComponentsToDisable { get; set; } = new();
+
+    public void OnNetworkSpawn(Connection owner)
+    {
+        UpdateComponents();
+    }
+
+    protected override void OnStart()
+    {
+        UpdateComponents();
+    }
+
+    protected override void OnEnabled()
+    {
+        UpdateComponents();
+    }
+
+    private void UpdateComponents()
+    {
+        foreach(var component in ComponentsToDisable)
+            component.Enabled = !IsProxy;
+    }
+}
