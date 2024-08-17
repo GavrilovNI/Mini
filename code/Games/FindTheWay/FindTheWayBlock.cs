58fd50c8c8fef330974b6a8493715b386fbecae4
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-08-17 17:11:28 +0200

    add FakeBlock

---
+++ code/Games/FindTheWay/FindTheWayBlock.cs
@@ -0,0 +1,23 @@
+﻿using Sandbox;
+
+namespace Mini.Games.FindTheWay;
+
+public abstract class FindTheWayBlock : Component
+{
+    [Property]
+    [RequireComponent]
+    public ModelRenderer ModelRenderer { get; set; } = null!;
+
+    [Property]
+    public Color Color { get; set; } = new Color(0.33f, 0.33f, 0.33f);
+
+    public virtual void Setup(FindTheWayGame game)
+    {
+        Color = game.Color;
+    }
+
+    protected override void OnStart()
+    {
+        ModelRenderer.Tint = Color;
+    }
+}
