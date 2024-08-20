002f8329befe6c1f259f9f24983ba0c3f8288e99
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-08-21 00:50:48 +0200

    set Consts.CubeModelSize to actually const insted of static readonly

--- code/Consts.cs
+++ code/Consts.cs
@@ -4,7 +4,7 @@ namespace Mini;
 
 public static class Consts
 {
-    public static readonly float CubeModelSize = 50f;
+    public const float CubeModelSize = 50f;
 
     public const int MinPlayersToPlay = 1;
 
