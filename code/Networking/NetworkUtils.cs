e071a939074864411c880c95b59c52cb242c419b
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-08-21 02:21:53 +0200

    allow connect to local client from menu

---
+++ code/Networking/NetworkUtils.cs
@@ -0,0 +1,33 @@
+﻿using Sandbox;
+using Sandbox.Network;
+using Sandbox.Utility;
+using System.Threading.Tasks;
+
+namespace Mini.Networking;
+
+public static class NetworkUtils
+{
+    public static async Task<bool> TryConnectToLocal(TaskSource taskSource)
+    {
+        GameNetworkSystem.Connect("local");
+        while(GameNetworkSystem.IsConnecting)
+            await taskSource.Yield();
+
+        bool connected = GameNetworkSystem.IsActive;
+
+        if(connected && Connection.Host.SteamId != Steam.SteamId)
+        {
+            GameNetworkSystem.Disconnect();
+            connected = false;
+        }
+
+        return connected;
+    }
+
+    public static Task<bool> TryConnectSteamId(TaskSource taskSource, ulong steamId, bool allowLocalConnection = false)
+    {
+        if(steamId == Steam.SteamId && allowLocalConnection)
+            return TryConnectToLocal(taskSource);
+        return GameNetworkSystem.TryConnectSteamId(steamId);
+    }
+}
